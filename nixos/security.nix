087ad6f120ac75c9b32e3660f0580cbf84aab06f
Sascha Grunert <sgrunert@suse.com>
2020-08-29 20:12:17 +0200

    Add auditd.conf
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- nixos/security.nix
+++ nixos/security.nix
@@ -9,4 +9,24 @@
       wheelNeedsPassword = false;
     };
   };
+
+  environment.etc = {
+    "audit/auditd.conf".text = ''
+      local_events = yes
+      write_logs = yes
+      log_file = /var/log/audit/audit.log
+      log_group = adm
+      log_format = RAW
+      flush = INCREMENTAL_ASYNC
+      freq = 50
+      max_log_file = 8
+      num_logs = 5
+      priority_boost = 4
+      disp_qos = lossy
+      name_format = NONE
+      max_log_file_action = ROTATE
+      space_left = 75
+      space_left_action = SYSLOG
+    '';
+  };
 }
