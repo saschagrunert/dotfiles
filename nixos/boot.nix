83e9741e994ef7b605610de0de91e8ea3099e3b2
Sascha Grunert <sgrunert@suse.com>
2020-10-08 21:41:00 +0200

    Change unprivileged ports for ipv4
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -19,9 +19,10 @@
     };
     kernel = {
       sysctl = {
-        "net.ipv4.conf.all.route_localnet" = 1;
         "net.ipv4.conf.all.forwarding" = 1;
+        "net.ipv4.conf.all.route_localnet" = 1;
         "net.ipv4.conf.default.forwarding" = 1;
+        "net.ipv4.ip_unprivileged_port_start" = 0;
       };
     };
     kernelModules = [ "kvm-intel" ];
