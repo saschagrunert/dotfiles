6d890a4fbf204c7ee5a15a5b9d55dba5a60db515
Sascha Grunert <sgrunert@suse.com>
2021-01-12 10:18:18 +0100

    Enable ibus
    Signed-off-by: Sascha Grunert <sgrunert@suse.com>

--- nixos/locale.nix
+++ nixos/locale.nix
@@ -1,9 +1,17 @@
 { config, lib, pkgs, ... }:
 {
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [ anthy ];
+    };
+  };
+
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de";
   };
+
   time.timeZone = "Europe/Berlin";
 }
