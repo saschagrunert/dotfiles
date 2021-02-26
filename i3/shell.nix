3ee2975886c339ed220d80a79daa5c1a468ad77e
Sascha Grunert <mail@saschagrunert.de>
2021-02-26 20:07:57 +0100

    Update i3pystatus
    Signed-off-by: Sascha Grunert <mail@saschagrunert.de>

--- i3/shell.nix
+++ i3/shell.nix
@@ -8,6 +8,12 @@ stdenv.mkDerivation {
   buildInputs = with pkgs; [
     libpulseaudio
     (i3pystatus.overrideAttrs (oldAttrs: {
+      src = fetchFromGitHub {
+        owner = "enkore";
+        repo = "i3pystatus";
+        rev = "7b861a42f575525e817cecd9bf309c881f2c5c3d";
+        sha256 = "1pl6lhdr1jma8if3j5qlnf9s0svk4n8a12bqwkyjwbfkq2nhmasw";
+      };
       propagatedBuildInputs = with python38Packages; [
         dbus-python
         pytz
