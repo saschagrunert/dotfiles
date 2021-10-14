071beb94986404bff0f4f3ccf229fc869baac932
Sascha Grunert <sgrunert@redhat.com>
2021-10-14 10:44:17 +0200

    Fix test syntax for $LD_LIBRARY_PATH
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,9 @@ set -x GIT_DISCOVERY_ACROSS_FILESYSTEM 1
 set -x QT_STYLE_OVERRIDE gtk2
 set -x MAN_POSIXLY_CORRECT 1
 
-if test -z $LD_LIBRARY_PATH
-    set -x LD_LIBRARY_PATH /run/current-system/sw/lib:(nix eval --raw nixpkgs.stdenv.cc.cc.lib)/lib
+if test -z "$LD_LIBRARY_PATH"
+    set -x LD_LIBRARY_PATH (nix eval --raw nixpkgs.stdenv.cc.cc.lib)/lib
+    set -a LD_LIBRARY_PATH /run/current-system/sw/lib
 end
 
 if test -z $PROTOC
