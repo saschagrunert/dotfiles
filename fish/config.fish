558f86e9772f4438cc695bfe97a6223459563895
Sascha Grunert <sgrunert@redhat.com>
2021-12-07 15:19:54 +0100

    Fix LD_LIBRARY_PATH
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,7 @@ set -x QT_STYLE_OVERRIDE gtk2
 set -x MAN_POSIXLY_CORRECT 1
 
 if test -z "$LD_LIBRARY_PATH"
-    set -x LD_LIBRARY_PATH (nix eval --raw nixpkgs.stdenv.cc.cc.lib)/lib
+    set -x LD_LIBRARY_PATH (nix eval --raw nixpkgs\#stdenv.cc.cc.lib)/lib
     set -a LD_LIBRARY_PATH /run/current-system/sw/lib
 end
 
