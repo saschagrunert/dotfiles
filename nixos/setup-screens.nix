ef3195d77ad05e2ecd3b5fb5cbdfcbb9665049ee
Sascha Grunert <sgrunert@redhat.com>
2025-02-24 14:42:29 +0100

    Update setup scripts
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nixos/setup-screens.nix
+++ nixos/setup-screens.nix
@@ -3,16 +3,17 @@ writeShellScriptBin "setup-screens" ''
   set -euxo pipefail
 
   LAPTOP=eDP-1
-  L=DP-3
+  L=DP-4
   R=DP-8
   MODE=3840x2160
+
+  # Disable them for now
+  xrandr --output $R --off
+  xrandr --output $L --off
 
   # Initial setup
   xrandr --output $R --mode $MODE --left-of $LAPTOP
   xrandr --output $L --mode $MODE --left-of $R
-
-  # Set the DPI
-  xrandr --dpi 150
 
   # Render the background
   feh --bg-scale ~/.dotfiles/wallpaper/blurred.jpg
