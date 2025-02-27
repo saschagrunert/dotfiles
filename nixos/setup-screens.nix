9ab9d90aa7667c120c3719e2f87ed85a694cd1be
Sascha Grunert <sgrunert@redhat.com>
2025-02-27 08:57:36 +0100

    Update screen setup script
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nixos/setup-screens.nix
+++ nixos/setup-screens.nix
@@ -7,15 +7,12 @@ writeShellScriptBin "setup-screens" ''
   R=DP-8
   MODE=3840x2160
 
-  # Disable them for now
-  xrandr --output $R --off
-  xrandr --output $L --off
-
   # Initial setup
   xrandr --output $R --mode $MODE --left-of $LAPTOP
   xrandr --output $L --mode $MODE --left-of $R
 
   # Render the background
   feh --bg-scale ~/.dotfiles/wallpaper/blurred.jpg
-''
 
+  xset dpms force off
+''
