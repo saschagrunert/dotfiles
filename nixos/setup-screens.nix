c541bb20720284c74dadad7bb1874cc0a15aaca0
Sascha Grunert <sgrunert@redhat.com>
2026-01-30 08:38:42 +0100

    Update screen setup script
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nixos/setup-screens.nix
+++ nixos/setup-screens.nix
@@ -2,21 +2,12 @@ with import <nixpkgs> { };
 writeShellScriptBin "setup-screens" ''
   set -euxo pipefail
 
-  LAPTOP=eDP
-  DT=DisplayPort
+  DT=DP
+  LAPTOP=eDP-1
+  L=$DT-3
+  R=$DT-8
   MODE=3840x2160
 
-  if xrandr --listmonitors | grep -q $DT-8; then
-      R=$DT-8
-  else
-      R=$DT-7
-  fi
-
-  if xrandr --listmonitors | grep -q $DT-3; then
-      L=$DT-3
-  else
-      L=$DT-2
-  fi
 
   # Initial setup
   xrandr --output $R --mode $MODE --left-of $LAPTOP
