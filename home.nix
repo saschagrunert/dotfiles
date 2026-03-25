6b87ab543eb6832d832d243eabe804ebc0ec7f74
Sascha Grunert <sgrunert@redhat.com>
2026-03-25 13:38:30 +0100

    make all xdg dirs lower case
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- home.nix
+++ home.nix
@@ -29,8 +29,14 @@ in
   xdg = {
     userDirs = {
       enable = true;
+      desktop = "$HOME/desktop";
+      documents = "$HOME/documents";
       download = "$HOME/downloads";
+      music = "$HOME/music";
       pictures = "$HOME/pictures";
+      publicShare = "$HOME/public";
+      templates = "$HOME/templates";
+      videos = "$HOME/videos";
     };
 
     configFile = {
