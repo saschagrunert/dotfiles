b7ddd8e1ab525f1d959a6734e74c6b33101f0222
Sascha Grunert <sgrunert@redhat.com>
2026-07-13 09:43:43 +0200

    Fix git aliases
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/functions/gup.fish
+++ fish/functions/gup.fish
@@ -1,6 +1,20 @@
-# Update branch from remote
+# Update branch from remote, sync origin, prune merged branches
 function gup
     set -l remote $argv[1]
     test -z "$remote" && set remote origin
-    git fetch $remote && git merge $remote/(gldb) && git push && gl
+    set -l default_branch (gldb)
+
+    git fetch --prune $remote \
+        && git merge $remote/$default_branch \
+        && git push \
+        || return $status
+
+    test "$remote" != origin && git fetch --prune origin || return $status
+
+    for branch in (git branch --merged $default_branch | grep -v '^\*' | string trim | grep -v "^$default_branch\$")
+        git branch -d $branch
+    end
+
+    echo
+    git branch
 end
