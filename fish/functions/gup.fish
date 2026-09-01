839742cd892b19f31535bf0d60cb2aa8168ecbe4
Sascha Grunert <sgrunert@redhat.com>
2026-09-01 10:28:44 +0200

    Prevent tag clobbering in gup fetch
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/functions/gup.fish
+++ fish/functions/gup.fish
@@ -4,13 +4,13 @@ function gup
     test -z "$remote" && set remote origin
     set -l default_branch (gldb)
 
-    git fetch --prune $remote \
+    git fetch --prune --no-tags $remote "+refs/heads/*:refs/remotes/$remote/*" \
         && git merge $remote/$default_branch \
         && git push \
         || return $status
 
     if test "$remote" != origin
-        git fetch --prune origin || return $status
+        git fetch --prune --no-tags origin "+refs/heads/*:refs/remotes/origin/*" || return $status
     end
 
     for branch in (git branch --merged $default_branch | grep -v '^\*' | string trim | grep -v "^$default_branch\$")
