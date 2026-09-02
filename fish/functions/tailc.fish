6d1cc76216f94f2e4b1b23c5db3060a7fec4b2e1
Sascha Grunert <sgrunert@redhat.com>
2026-09-02 15:58:03 +0200

    Fix shell injection, improve power script, and clean up configs
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/functions/tailc.fish
+++ fish/functions/tailc.fish
@@ -5,6 +5,5 @@ function tailc
         return 1
     end
     set -l lines (math (tput lines) - 2)
-    set -l escaped_url (string replace -a "'" "'\\''" -- $argv[1])
-    watch -n1 "curl -sf '$escaped_url' | tail -n $lines"
+    watch -n1 -- curl -sf -- $argv[1] \| tail -n $lines
 end
