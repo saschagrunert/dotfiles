addd22249ae5cec28b1603556db5ee39d915de23
Sascha Grunert <sgrunert@redhat.com>
2026-09-09 13:38:06 +0200

    Fix nvim proselint error and improve tmux status bar
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -7,7 +7,6 @@ return {
       lint.linters_by_ft = {
         bash = { "shellcheck" },
         go = { "golangcilint" },
-        markdown = { "proselint" },
         nix = { "statix" },
         python = { "ruff" },
         sh = { "shellcheck" },
