17f336cc9bde8343598419cf1fc44aa22832d1a2
Sascha Grunert <sgrunert@redhat.com>
2026-09-11 13:08:28 +0200

    Clean boot partition before nixos-rebuild
    Signed-off-by: Sascha Grunert <sgrunert@redhat.com>

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,6 +1,7 @@
 function up -d "Update system"
     nix flake update --flake $DOTFILES
     and sudo nix-collect-garbage -d
+    and sudo /nix/var/nix/profiles/system/bin/switch-to-configuration boot
     and sudo nixos-rebuild switch --flake $DOTFILES#nixos
     and sudo nix-collect-garbage -d
     and rustup update
